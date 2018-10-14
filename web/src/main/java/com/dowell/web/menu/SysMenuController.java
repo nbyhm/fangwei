595d3c2ac3a044eff9a2554fdd6752486c648f89
boyd1915 <nanbo@baiseng.com>
2018-10-14 23:13:25 +0800

    添加测试内容

--- web/src/main/java/com/dowell/web/menu/SysMenuController.java
+++ web/src/main/java/com/dowell/web/menu/SysMenuController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -34,6 +35,11 @@ public class SysMenuController extends AbstractController {
     public Result nav(){
         List<SysMenu> menuList = sysMenuService.getUserMenuList(1L);
         return ResultGenerator.genSuccessResult(menuList);
+    }
+
+    @PostMapping("test")
+    public void test(){
+        System.out.println("测试");
     }
 
 }
