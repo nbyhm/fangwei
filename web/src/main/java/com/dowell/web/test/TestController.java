2071d496bf9b61671f655ed0703b63f78a12308c
boyd1915 <nanbo@baiseng.com>
2018-10-16 14:49:27 +0800

    SpringBoot自定义favicon.ico

--- web/src/main/java/com/dowell/web/test/TestController.java
+++ web/src/main/java/com/dowell/web/test/TestController.java
@@ -7,10 +7,11 @@ import com.dowell.web.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.thymeleaf.util.DateUtils;
 
 import java.util.HashMap;
@@ -22,7 +23,7 @@ import java.util.Map;
  * @description 测试
  * @create 2018-09-28
  **/
-@RestController
+@Controller
 @RequestMapping("/test")
 @Api(tags = "测试类")
 public class TestController extends BaseController {
@@ -30,6 +31,7 @@ public class TestController extends BaseController {
     @Autowired
     UserService userService;
 
+    @ResponseBody
     @GetMapping("/list")
     @ApiOperation("查询所有用户信息")
     public ResponseBo index(){
@@ -39,6 +41,13 @@ public class TestController extends BaseController {
         map.put("当前时间", DateUtils.createNow());
         map.put("list",list);
         return ResponseBo.ok(map);
+    }
+
+
+    @RequestMapping(value = "/index")
+    public String index(Model model) {
+        model.addAttribute("name","SpringBootFavicon");
+        return "views/index";
     }
 
 }
